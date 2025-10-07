c4c6312ab7ea431fa7fe68617db7910a9efe8284
SMaryushenkov <SMaryushenkov@croc.ru>
2025-10-07 19:32:50 +0300

    init

---
+++ src/test/java/ru/alligator/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alligator.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
