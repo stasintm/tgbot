d2d81c5d63b723fe1f1c8fe8f88c9f6af9198697
Stas <stasintm@gmail.com>
2025-10-29 00:19:31 +0300

    согласование отпуска с использованием kafka

--- src/main/java/ru/alligator/bot/storage/ChatStateStorage.java
+++ src/main/java/ru/alligator/bot/storage/ChatStateStorage.java
@@ -1,8 +1,12 @@
 package ru.alligator.bot.storage;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ChatStateStorage {
     boolean hasSate(Long chatId);
     ChatState getState(Long chatId);
     void putState(ChatState state);
+    List<ChatState> getByEmployeeId(UUID employeeId);
 }
 
